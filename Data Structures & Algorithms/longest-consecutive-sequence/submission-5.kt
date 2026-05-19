ea99ccc915112769d51044b6f07d1b46db98396e
KianNingT <91300653+KianNingT@users.noreply.github.com>
2026-05-19 22:45:46 +0800

    Add: longest-consecutive-sequence - submission-5

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-5.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun longestConsecutive(nums: IntArray): Int {
+         if (nums.isEmpty()) {
+            return 0
+        }
+ val numSet = nums.toSet()
+        var maxVal = 1
+        for (num in numSet) {
+            if (!numSet.contains(num - 1)) {
+                var start = 1
+                //start
+                while (num + start in numSet) {
+                    start++
+                    maxVal = maxOf(start, maxVal)
+                }
+            }
+        }
+        return maxVal
+    }
+}
