106ec2186cee28c174c7664577e036fefe437cde
KianNingT <91300653+KianNingT@users.noreply.github.com>
2026-05-19 22:22:33 +0800

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.kt
@@ -0,0 +1,40 @@
+class Solution {
+    fun isValidSudoku(board: Array<CharArray>): Boolean {
+ for (row in 0 until 9) {
+            val seenSet = mutableSetOf<String>()
+            for (col in 0 until 9) {
+                val each = board[row][col].toString()
+                if (!seenSet.add(each) && each != ".") {
+                    return false
+                }
+            }
+        }
+
+        for (row in 0 until 9) {
+            val seenSet = mutableSetOf<String>()
+           for (col in 0 until 9) {
+                val each = board[col][row].toString()
+                if (!seenSet.add(each) && each != ".") {
+                    return false
+                }
+            }
+        }
+
+        for (grid in 0 until 9) {
+            val seenSet = mutableSetOf<String>()
+            for (i in 0 until 3) {
+                for (j in 0 until 3) {
+
+                 val row = (grid / 3) * 3 + i
+                    val col = (grid % 3) * 3 + j
+                    val each = board[row][col].toString()
+                    if (!seenSet.add(each) && each != ".") {
+                        return false
+                    }
+                }
+            }
+        }
+
+        return true
+    }
+}
