481593eada39efd019ac47ec29baf8d683acb328
KianNingT <91300653+KianNingT@users.noreply.github.com>
2026-05-20 22:42:09 +0800

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.kt
@@ -0,0 +1,28 @@
+class Solution {
+    fun isPalindrome(s: String): Boolean {
+    if (s.isEmpty()) {
+            return true
+        }
+
+        var left = 0
+        var right = s.length - 1
+
+        while (left < right) {
+            if (!s[left].isLetterOrDigit() || s[left] == ' ') {
+                left++
+                continue
+            }
+            if (!s[right].isLetterOrDigit() || s[right] == ' ') {
+                right--
+                continue
+            }
+            if (s[left].equals(s[right], ignoreCase = true)) {
+                left++
+                right--
+            } else {
+                return false
+            }
+        }
+        return true
+}
+}
