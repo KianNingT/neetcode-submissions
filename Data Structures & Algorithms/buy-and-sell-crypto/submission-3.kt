8df3ad812aa16857ec457a36a56c225c9e690c78
KianNingT <91300653+KianNingT@users.noreply.github.com>
2026-06-06 22:32:27 +0800

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun maxProfit(prices: IntArray): Int {
+ var highestProfit = 0
+        var lowestPrice = Int.MAX_VALUE
+
+        for (price in prices) {
+            if (price < lowestPrice) {
+                lowestPrice = price
+            } else {
+                val todayProfit = price - lowestPrice
+                highestProfit = maxOf(highestProfit, todayProfit)
+            }
+        }
+        return highestProfit
+    }
+}
