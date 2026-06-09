cb1124ebe6cc63b7374018a2b928210f441d4429
KianNingT <91300653+KianNingT@users.noreply.github.com>
2026-06-09 22:13:52 +0800

    Add: three-integer-sum - submission-4

---
+++ Data Structures & Algorithms/three-integer-sum/submission-4.kt
@@ -0,0 +1,47 @@
+class Solution {
+    fun threeSum(nums: IntArray): List<List<Int>> {
+if (nums.isEmpty()) {
+            return listOf()
+        }
+        val res = mutableListOf<List<Int>>()
+
+        nums.sort()
+
+
+        for (i in nums.indices) {
+            val curr = nums[i]
+            if (i > 0 && curr == nums[i - 1]) {
+                continue
+            }
+
+            var left = i + 1
+            var right = nums.size - 1
+
+            while (left < right) {
+
+                val sum = curr + nums[left] + nums[right]
+
+                if (sum < 0) {
+                    left++
+                    continue
+                } else if (sum > 0) {
+                    right--
+                    continue
+                } else if (sum == 0) {
+                    res.add(listOf(curr, nums[left], nums[right]))
+
+                    val usedLeft = nums[left]
+                    while (left < right && usedLeft == nums[left]) {
+                        left++
+                    }
+
+                    val usedRight = nums[right]
+                    while (left < right && usedRight == nums[right]) {
+                        right--
+                    }
+                }
+            }
+        }
+        return res
+    }
+}
