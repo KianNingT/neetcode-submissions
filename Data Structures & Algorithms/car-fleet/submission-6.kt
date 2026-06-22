be15785bb494d235499178249be6a3415341de50
KianNingT <91300653+KianNingT@users.noreply.github.com>
2026-06-22 20:46:35 +0800

    Add: car-fleet - submission-6

---
+++ Data Structures & Algorithms/car-fleet/submission-6.kt
@@ -0,0 +1,18 @@
+class Solution {
+    fun carFleet(target: Int, position: IntArray, speed: IntArray): Int {
+        val carPairs = position.zip(speed).sortedByDescending { it.first }
+
+        var maxTime = 0.0
+        var carFleet = 0
+
+        for ((pos, spd) in carPairs) {
+            val timeNeeded = (target - pos).toDouble() / spd
+            if (timeNeeded > maxTime) {
+                carFleet++
+                maxTime = timeNeeded
+                
+            }
+        }
+        return carFleet
+    }
+}
