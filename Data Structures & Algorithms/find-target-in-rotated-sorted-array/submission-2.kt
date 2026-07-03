5be4a2d20f6eb4cfbd9faf61d6004bbf78141870
KianNingT <91300653+KianNingT@users.noreply.github.com>
2026-07-04 00:50:57 +0800

    Add: find-target-in-rotated-sorted-array - submission-2

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-2.kt
@@ -0,0 +1,32 @@
+class Solution {
+    fun search(nums: IntArray, target: Int): Int {
+        var left = 0
+        var right = nums.size - 1
+
+        while (left <= right) {
+            val mid = left + ((right - left)/ 2)
+
+            if (target == nums[mid]) {
+                return mid
+            }
+            //are we in left or right sorted portion?
+
+            if (nums[mid] >= nums[left]) {
+                //left sorted
+                if (target > nums[mid] || target < nums[left]) {
+                    left = mid + 1
+                } else {
+                    right = mid - 1
+                }
+            } else {
+                //right sorted portion
+                if (target < nums[mid] || target > nums[right]) {
+                    right = mid - 1
+                } else {
+                    left = mid + 1
+                }
+            }
+        }
+        return -1
+    }
+}
