8ba105b994264b15c90d672ab77f0eed14c5a496
KianNingT <91300653+KianNingT@users.noreply.github.com>
2026-07-13 23:05:04 +0800

    Add: max-water-container - submission-8

---
+++ Data Structures & Algorithms/max-water-container/submission-8.kt
@@ -0,0 +1,21 @@
+class Solution {
+    fun maxArea(heights: IntArray): Int {
+        var left = 0
+        var right = heights.size - 1
+        var maxArea = 0
+
+        while (left < right) {
+
+            val length = right - left
+            val breadth = minOf(heights[left], heights[right])
+            maxArea = maxOf(maxArea, length * breadth)
+            
+            if (heights[left] <= heights[right]) {
+                left++ 
+            } else {
+                right--
+            }
+        }
+        return maxArea
+    }
+}
