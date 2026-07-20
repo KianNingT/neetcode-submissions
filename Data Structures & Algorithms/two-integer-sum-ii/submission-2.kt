23b47434b114af3c602135766e1adc0444c37916
KianNingT <91300653+KianNingT@users.noreply.github.com>
2026-07-21 00:41:18 +0800

    Add: two-integer-sum-ii - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-2.kt
@@ -0,0 +1,23 @@
+class Solution {
+    fun twoSum(numbers: IntArray, target: Int): IntArray {
+ var left = 0
+    var right = numbers.size - 1
+    
+    while (left < right) {
+        
+        val sum = numbers[left] + numbers[right]
+        if (sum == target) {
+            return intArrayOf(left + 1, right + 1)
+        }
+        
+        if (sum < target) {
+            left++
+            continue
+        } else {
+            right--
+            continue
+        }
+    }
+    return intArrayOf()
+    }
+}
