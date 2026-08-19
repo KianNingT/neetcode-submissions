9922370067ff2a5ed7a2840160140f55e93b6551
KianNingT <91300653+KianNingT@users.noreply.github.com>
2026-08-19 21:47:01 +0800

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.kt
@@ -0,0 +1,27 @@
+class Solution {
+    fun isAnagram(s: String, t: String): Boolean {
+
+        if (s.length != t.length) {
+            return false
+        }
+
+        val seenMap = mutableMapOf<Char, Int>()
+        for (eachChar in s) {
+            seenMap[eachChar] = seenMap.getOrDefault(eachChar, 0) + 1
+        }
+
+        for (eachChar in t) {
+            seenMap[eachChar] = seenMap.getOrDefault(eachChar, 0) - 1
+            if (seenMap[eachChar]!! < 0) {
+                return false
+            }
+            if (seenMap[eachChar] == 0) {
+                seenMap.remove(eachChar)
+            }
+        }
+        if (seenMap.isEmpty()) {
+            return true
+        }
+        return false
+    }
+}
